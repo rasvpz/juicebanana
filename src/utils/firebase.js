8932b5f31ea24d5e52c0346765d5a7ab243fa8bd
Abdul Rasheed <rasheed@adamtechnologies.io>
2024-08-23 17:31:20 +0530

    correct the error depreciation warning

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA14aq2pHCkuZZ8N4bD4MzrKQtF9NluJ5A",
   authDomain: "juicebanana.firebaseapp.com",
@@ -19,5 +15,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const auth = getAuth();
+// Removed the unused analytics variable
+export const auth = getAuth(app); // Added app as an argument
